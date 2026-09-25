3845dd3d5cfeb642790dee88f363ab922ff57eb0
Jatin Panchal <b250260@skit.ac.in>
2026-09-25 08:28:19 +0530

    saving error

--- server/apply_sql.ts
+++ server/apply_sql.ts
@@ -1,6 +1,5 @@
 import postgres from 'postgres';
 import fs from 'fs';
-import { ENV } from './_core/env.ts';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -8,7 +7,7 @@ dotenv.config();
 async function run() {
   const sqlContent = fs.readFileSync('drizzle/0004_add_attendance_records.sql', 'utf-8');
   console.log('Connecting to', process.env.DATABASE_URL);
-  const sql = postgres(process.env.DATABASE_URL);
+  const sql = postgres(process.env.DATABASE_URL!);
   try {
     await sql.unsafe(sqlContent);
     console.log('Successfully applied migration');
